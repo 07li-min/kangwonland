192a1617953d6d9ec83bb6d4bc085145f9c8814e
unknown <godgodlim@knou.ac.kr>
2022-01-07 15:33:12 +0900

    최종수정

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
 $(document).ready(function(){
-    // 윈도우 창 크기 변화 감지
-    $( window ).resize(function(){
-      var windowWidth = $( window ).width();
-      console.log('크기변화 = ' + windowWidth + '입니다');
-    });
 
-  // pc메뉴 hover
   $('.main-hd .pc-hd .dep1').hover(function(){
     $('.main-hd').toggleClass('on')
     $('.top-hd').toggleClass('on')
   });
 
 
-  // 모바일메뉴 : 아코디언
-  var dep1 = '.m-dep1',
+  let dep1 = '.m-dep1',
   dep1Target = '.m-dep1 .tit',  
   dep2Target = '.m-dep2 >li ', 
   dep3Target = '.m-dep3 > li';
@@ -24,35 +17,23 @@ $(document).ready(function(){
   $(dep1Target).on('click', function(e){
     console.log('on')
     e.preventDefault();
-    // css 상태만 변경
-    $(this).closest(dep1).toggleClass('active').siblings().removeClass('active');
-    // 움직임 효과
     $(this).closest(dep1).find(dep2Target).slideToggle();
-    console.log('dep1')
     $(this).closest(dep1).siblings().find(dep2Target).slideUp();
   })
 
   $(dep2Target).on('click', function(e){
     e.preventDefault();
-    // css 상태만 변경
     $(this).toggleClass('active').siblings().removeClass('active');
-    // 움직임 효과
     $(this).find(dep3Target).slideToggle();
     $(this).closest(dep2Target).siblings().find(dep3Target).slideUp();
   });
 
-
-
-
-  // 모바일메뉴_버튼
   $('.mob-btn').click(function(){
     $(this).toggleClass('active'); 
     $('.hd .mob-hd').toggleClass('on'); 
     $('html, body').toggleClass('hide-scroll');
   });
 
-
-  // mob-banner
   var mobswiper = new Swiper(".mob-banner", {
     slidesPerView: 1,
     loop: true,
@@ -70,7 +51,6 @@ $(document).ready(function(){
     },
   });
 
-  // 비지니스슬라이드
   var businessswiper = new Swiper(".right", {
     slidesPerView: 1,
     autoHeight : true,
@@ -84,26 +64,13 @@ $(document).ready(function(){
       nextEl: ".reveal .swiper-button-next",
       prevEl: ".reveal .swiper-button-prev",
     },
-    // breakpoints: { 
-    //   640: { 
-    //     640 이상일 경우
-    //     width: 500,
-    //   },
-    //   768: {
-    //     width: 650,
-    //   },
-    //   1024: {
-    //     width: 750,
-    //   },
-    // }
   });
 
-  // 비지니스 슬라이드(텍스트) 
-  var arrowPrev = '.business .reveal .swiper-button-prev',
+  let arrowPrev = '.business .reveal .swiper-button-prev',
   arrowNext = '.business .reveal .swiper-button-next';
 
   $(arrowNext).on('click', function() {
-    var currentActive = $('.business .left .box li.active'); 
+    let currentActive = $('.business .left .box li.active'); 
     currentActive.removeClass('active'); 
   
     if (currentActive.is(':last-child')) {
@@ -114,7 +81,7 @@ $(document).ready(function(){
   });
 
   $(arrowPrev).on('click', function() {
-    var currentActive = $('.business .left .box li.active'); 
+    let currentActive = $('.business .left .box li.active'); 
     currentActive.removeClass('active'); 
   
     if (currentActive.is(':first-child')) {
@@ -124,34 +91,50 @@ $(document).ready(function(){
     }
   });
 
+
   // ft
   $('.family').click(function(){
-    $(this).toggleClass('active')
+    $(this).toggleClass('active');
   });
 
-  //팝업
-  //head 태그 안에 스크립트 선언
-  
-//head 태그 안에 스크립트 선언
-function setCookie( name, value, expiredays ) {
-  var todayDate = new Date();
-  todayDate.setDate( todayDate.getDate() + expiredays ); 
-  document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-}
-function closePop() {
-  if ( document.pop_form.chkbox.checked ){
-      setCookie( "maindiv", "done" , 1 );
+  // popup
+  $('.close_pop').click(function(){
+    console.log('닫기', this);
+    let tmpPop = $(this).closest('.popup');
+    closePop(tmpPop);
+  });
+
+  function setCookie( name, value, expiredays ) {
+    // console.log('setCookie', name);
+    let todayDate = new Date();
+    todayDate.setDate( todayDate.getDate() + expiredays ); 
+    document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";";
+    // console.log('set Cookie', document.cookie);
   }
-  document.all['layer_popup'].style.visibility = "hidden";
-}
-cookiedata = document.cookie;   
-if ( cookiedata.indexOf("maindiv=done") < 0 ){     
-    document.all['layer_popup'].style.visibility = "visible";
-}
-else {
-    document.all['layer_popup'].style.visibility = "hidden";
-}
 
+  function closePop(popElem) {
+    if ( popElem.find('form input').is(':checked') ){
+      setCookie( popElem[0].id , "done" , 1 );
+    }
+    popElem.removeClass('active');
+  }
 
+  cookiedata = document.cookie; 
+  if ( cookiedata.indexOf("popup_1=done") < 0 ){     
+    $('#popup_1').addClass('active');
+  } else {
+    $('#popup_1').removeClass('active');
+  }
+  if ( cookiedata.indexOf("popup_2=done") < 0 ){     
+    $('#popup_2').addClass('active');
+  } else {
+    $('#popup_2').removeClass('active');
+  }
   
+
+
+
+
+  // wow
+  new WOW().init();
 });
